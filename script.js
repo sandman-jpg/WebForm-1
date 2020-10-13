996a2b8db7c3dc0d80ce6f7f33d2c49da0169372
Rachel Amin <rami4179@uni.sydney.edu.au>
2020-10-13 01:02:38 +0000

    Inserted the javascript code into script.js ; stylised the sheet

--- script.js
+++ script.js
@@ -2,6 +2,24 @@
 ///////////////////////////////////////////////////////////////////
 
 // Insert your setup and draw functions here
+function setup(){
+  // Creates a canvas with the same dimensions as the browser window
+  var canvas = createCanvas(window.innerWidth, window.innerHeight);
+  // Attaches the canvas to the body element with the ID 'canvas-container'
+  canvas.parent('canvas-container');
+  
+}
+
+function draw(){
+  
+  // Sets the canvas background every frame
+  background(230, 255, 230);
+
+  fill("red");
+  ellipse(mouseX, mouseY, 50);
+}
+
+
 
 
 // REGULAR JAVASCRIPT FUNCTIONS
